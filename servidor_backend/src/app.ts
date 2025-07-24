9283aaeb42c1b88ee36481d61f00eb7bedb80b1d
rodrigo <martinqueve60@gmail.com>
2025-07-24 02:02:13 -0300

    productos router

--- servidor_backend/src/app.ts
+++ servidor_backend/src/app.ts
@@ -2,15 +2,24 @@
 // Express es un framework simple que permite manejar requests a endpoints con app.[metodo]()
 // Tambien permite ejecutar middlewares con app.use()
 
+import express from "express";
 import { Request, Response } from "express";
 
-import express from "express";
+import router from './routes/index'
 import logTime from "./middleware/logTime";
+
+
 
 const app = express();
 
+console.log('primero va esto')
 app.use(logTime)
 
+console.log('luego esto otro')
+app.use(router)
+
+
+console.log('y al final, esto')
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello, TypeScript + Express!');
 });
