c131bcc4e1cdd7d8dd6b666de3f95c5cc06cf23d
rodrigo <martinqueve60@gmail.com>
2025-08-02 10:48:44 -0300

    setup variables de entorno

--- servidor_backend/src/server.ts
+++ servidor_backend/src/server.ts
@@ -7,4 +7,5 @@ const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+  console.log('env variable test: ', process.env.MI_VARIABLE)
 });
