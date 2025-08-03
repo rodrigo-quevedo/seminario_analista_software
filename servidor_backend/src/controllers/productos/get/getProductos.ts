751c6dc977e8daa1bc7fb8f4decc9b4fb448c047
rodrigo <martinqueve60@gmail.com>
2025-08-03 16:16:50 -0300

    features: GET /productos Controller

--- servidor_backend/src/controllers/productos/get/getProductos.ts
+++ servidor_backend/src/controllers/productos/get/getProductos.ts
@@ -1,5 +1,10 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
+import ProductoDAO from "@daos/productos/ProductoDAO";
 
-export function getProductos(req: Request, res: Response){
-    console.log("DATOS DE TODOS LOS PRODUCTOS")
+export async function getProductos(req: Request, res: Response){
+
+    let arrProductos = await ProductoDAO.getProductos();
+
+    res.json({productos: arrProductos });// {productos{[{//producto1}, {producto2}//etc]}}
+
 };
